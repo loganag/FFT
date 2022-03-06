d0758d2cf836562c653a4f3b802b74e2a42e9c4b
loganag <leha.pushkar@gmail.com>
2022-03-06 13:59:35 +0300

    Rename classes

--- src/main/java/com/example/FFT/FFTApplication.java
+++ src/main/java/com/example/FFT/FFTApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class FFTApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainApp.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FFTApplication.class.getResource("mainApp.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 900);
         stage.setTitle("Быстрое преобразование Фурье");
         stage.setScene(scene);
